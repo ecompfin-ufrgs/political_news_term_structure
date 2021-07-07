099efba71ea0f6ab9393381bc3fa5674109f2ef8
Bernardo Paulsen <bernardohpaulsen@gmail.com>
2021-07-07 13:50:18 -0300

    Add scrapy project

---
+++ scrapy/news/items.py
@@ -0,0 +1,17 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import Item, Field
+
+
+class NewsItem(Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    #link  = Field()
+    #date  = Field()
+    #title = Field()
+    #text  = Field()
+    pass
+
