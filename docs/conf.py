a64fe5774b4a71544eb7af690779e03295412095
Bernardo Paulsen <bernardohpaulsen@gmail.com>
2021-07-23 21:22:26 -0300

    Add new directory to conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../news-brazil'))
+sys.path.insert(0, os.path.abspath('../selenium'))
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
