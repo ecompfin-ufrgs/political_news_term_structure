d7591753e11a0d2354e9fe94e2bd0a40a53ef52b
Bernardo Paulsen <bernardopaulsen@gmail.com>
2021-07-28 01:19:21 +0000

    Created Ig class.

--- selenium/ig.py
+++ selenium/ig.py
@@ -1,1 +1,50 @@
-#https://ultimosegundo.ig.com.br/politica/
+"""
+Title       : Minas 
+Description : Defines class Minas, child of Scraper, which scrapes the political section of Estado de Minas website.
+Author      : Bernardo Paulsen
+Version     : 1.0.0
+"""
+from   scraper import Scraper
+
+#import line_profiler
+#import atexit
+#profile = line_profiler.LineProfiler()
+#atexit.register(profile.print_stats)
+
+class Ig(Scraper):
+    """
+    Class, child of Scraper, which scrapes the political section of Estado de Minas website.
+    """
+    start_url   = "https://ultimosegundo.ig.com.br/politica/"
+    next_xpath  = "//a[@class='proxima']"
+    row_type    = "div"
+    row_class   = "empilhaImg"
+    title_xpath = "h2"
+    date_xpath  = "cite"
+    n_last      = 100
+    n_next      = 10
+    log_file    = "uol"
+    db_name     = "news.db"
+    db_table    = "uol"
+    
+    def get_row_xpath(self):
+        return f"descendant-or-self::{self.row_type}[@class='{self.row_class}' and position()>last()-{self.n_last}]"
+    
+    @staticmethod
+    def get_date(
+        date : str):
+        """
+        Gets date string and formats it to DATETIME data type (SQLite3).
+
+        :param date: Date string
+        :type date: str
+
+        :return: Formatted date
+        :rtype: str
+        """
+        return f f"{date[14:]}-{date[11:13]}-{date[8:10]} {date[:2]}:{date[3:5]}:00"
+
+
+if __name__ == "__main__":
+    c = Ig()
+    c.run()
