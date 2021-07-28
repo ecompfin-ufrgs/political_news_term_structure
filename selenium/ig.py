fa13b48aa5a486f3ab1160da37a682614becbe8a
Bernardo Paulsen <bernardopaulsen@gmail.com>
2021-07-28 01:21:34 +0000

    Ig failed scraping attempt.

--- selenium/ig.py
+++ selenium/ig.py
@@ -18,7 +18,7 @@ class Ig(Scraper):
     start_url   = "https://ultimosegundo.ig.com.br/politica/"
     next_xpath  = "//a[@class='proxima']"
     row_type    = "div"
-    row_class   = "empilhaImg"
+    row_class   = "empilhaImg-text"
     title_xpath = "h2"
     date_xpath  = "cite"
     n_last      = 100
@@ -42,7 +42,7 @@ class Ig(Scraper):
         :return: Formatted date
         :rtype: str
         """
-        return f f"{date[14:]}-{date[11:13]}-{date[8:10]} {date[:2]}:{date[3:5]}:00"
+        return f"{date[14:]}-{date[11:13]}-{date[8:10]} {date[:2]}:{date[3:5]}:00"
 
 
 if __name__ == "__main__":
