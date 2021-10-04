998fb468c5bab2d4f06293fbb080306de6321777
Bernardo Paulsen <bpaulsen@ws-bpaulsen.int.mbochip.com>
2021-10-03 23:16:21 -0300

    Added docstrings to Logger class and an __init__ file to scraper package.

--- scraping/logger.py
+++ scraping/logger.py
@@ -1,45 +1,51 @@
-"""
-Title: Logger Class
-Description: Class which manages a custom logger.
-Version: 0.0.1
-Author: Bernardo Paulsen
-"""
-
-
 import logging
 
 
 class Logger:
-    
-    MODE = "a"
-    LEVEL = logging.DEBUG
-    FORMAT = "%(asctime)s::%(name)s::%(levelname)s::%(message)s"
+    """
+    Implements custom logger.
+    """
+    MODE: str = "a"
+    LEVEL: int = logging.DEBUG
+    FORMAT: str = "%(asctime)s::%(name)s::%(levelname)s::%(message)s"
     
     def __init__(self,
-        name,
-        filename):
+                 name: str,
+                 file_name: str
+                 ) -> None:
+        """
+        Initiator method.
+
+        :param name: Logger name
+        :type name: str
+        :param file_name: Log file name
+        :type file_name: str
+        """
+        self.name: str = name
+        self.file_name: str = file_name
+        
+        self.logger: logging.Logger = self.get_logger()
+        
+        self.debug: logging.Logger.debug = self.logger.debug
+        self.info: logging.Logger.info = self.logger.info
+        self.warning: logging.Logger.warning = self.logger.warning
+        self.error: logging.Logger.error = self.logger.error
+        self.critical: logging.Logger.critical = self.logger.critical
+        
+    def get_logger(self) -> logging.Logger:
+        """
+        Returns custom logger with stream and file handlers.
             
-        self.name = name
-        self.filename = filename
-        
-        self.logger = self.get_logger()
-        
-        self.debug = self.logger.debug
-        self.warning = self.logger.warning
-        self.error = self.logger.error
-        
-    def get_logger(self):
-        
+        :return: Custom logger
+        :rtype: logging.Logger
+        """
         formatter = logging.Formatter(self.FORMAT)
         logger = logging.getLogger(self.name)
         logger.setLevel(self.LEVEL)
-        
         stream_handler = logging.StreamHandler()
-        file_handler = logging.FileHandler(self.filename, self.MODE)
-        
+        file_handler = logging.FileHandler(self.file_name, self.MODE)
         for handler in [stream_handler, file_handler]:
             handler.setLevel(self.LEVEL)
             handler.setFormatter(formatter)
             logger.addHandler(handler)
-            
         return logger
