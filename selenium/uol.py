9f497e78cbc50aefb29787d1acc245c38c2b0056
bpaulsen <bernardo.paulsen@mbochip.com>
2022-03-16 16:01:55 -0300

    More refactoring.

--- selenium/uol.py
+++ selenium/uol.py
@@ -4,37 +4,38 @@ Description : Defines class Minas, child of Scraper, which scrapes the political
 Author      : Bernardo Paulsen
 Version     : 1.0.0
 """
-from   scraper import Scraper
+from scraper import Scraper
 
-#import line_profiler
-#import atexit
-#profile = line_profiler.LineProfiler()
-#atexit.register(profile.print_stats)
+
+# import line_profiler
+# import atexit
+# profile = line_profiler.LineProfiler()
+# atexit.register(profile.print_stats)
 
 class Uol(Scraper):
     """
     Class, child of Scraper, which scrapes the political section of Estado de Minas website.
     """
-    start_url   = "https://noticias.uol.com.br/politica/"
-    next_xpath  = "button[class='btn-search align-center ver-mais btn-more btn btn-large btn-primary']"
-    row_type    = "div"
-    row_class   = "thumbnails-item align-horizontal list col-xs-8 col-sm-12 small col-sm-24 small"
+    start_url = "https://noticias.uol.com.br/politica/"
+    next_xpath = "button[class='btn-search align-center ver-mais btn-more btn btn-large btn-primary']"
+    row_type = "div"
+    row_class = "thumbnails-item align-horizontal list col-xs-8 col-sm-12 small col-sm-24 small"
     title_xpath = "h3"
-    date_xpath  = "time"
-    n_last      = 200
-    n_next_max  = 10
-    n_load_max  = 50
+    date_xpath = "time"
+    n_last = 200
+    n_next_max = 10
+    n_load_max = 50
     n_error_max = 20
-    log_file    = "uol9css"
-    db_name     = "news.db"
-    db_table    = "uol9css"
-    
+    log_file = "uol9css"
+    db_name = "news.db"
+    db_table = "uol9css"
+
     def get_row_xpath(self):
         return f"descendant-or-self::{self.row_type}[@class='{self.row_class}' and position()>last()-{self.n_last}]"
-    
+
     @staticmethod
     def get_date(
-        date : str):
+            date: str):
         """
         Gets date string and formats it to DATETIME data type (SQLite3).
 
--- selenium/zero.py
+++ selenium/zero.py
@@ -1,32 +1,32 @@
-import os
-from   scraper import Scraper
+from scraper import Scraper
+
 
 class Zero(Scraper):
-    start_url    = "https://gauchazh.clicrbs.com.br/politica/ultimas-noticias/"
-    next_xpath   = "descendant-or-self::button[@class='btn-show-more']"
-    row_type     = "div"
-    row_class    = "article-card__summary"
-    title_xpath  = "h2"
-    date_xpath   = "*[class='m-crd-pt__publish-date']"
-    n_last       = 50
-    n_next       = 10
-    log_file     = "zero"
-    db_name      = "news.db"
-    db_table     = "zero"
-        
+    start_url = "https://gauchazh.clicrbs.com.br/politica/ultimas-noticias/"
+    next_xpath = "descendant-or-self::button[@class='btn-show-more']"
+    row_type = "div"
+    row_class = "article-card__summary"
+    title_xpath = "h2"
+    date_xpath = "*[class='m-crd-pt__publish-date']"
+    n_last = 50
+    n_next = 10
+    log_file = "zero"
+    db_name = "news.db"
+    db_table = "zero"
+
     def get_row_xpath(self):
         return f"//{self.row_type}[@class='{self.row_class}' and position()>last()-{self.n_last}]"
 
     @staticmethod
     def get_date(date):
-        lst  = date.split()
-        day  = lst[0]
+        lst = date.split()
+        day = lst[0]
         time = lst[2]
-        hm   = time.split("h")
-        h    = hm[0]
-        h    = h if len(h) == 2 else f"0{h}"
-        m    = hm[1].split("min")[0]
-        m    = m if len(m) == 2 else f"0{m}"
+        hm = time.split("h")
+        h = hm[0]
+        h = h if len(h) == 2 else f"0{h}"
+        m = hm[1].split("min")[0]
+        m = m if len(m) == 2 else f"0{m}"
         return f"{day[6:]}-{day[3:5]}-{day[:2]} {h}:{m}:00"
 
 
