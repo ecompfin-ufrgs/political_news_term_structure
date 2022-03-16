3e9f1adeb23724cbb7b7b710e001feee5db49ed6
bpaulsen <bernardo.paulsen@mbochip.com>
2022-03-16 15:57:23 -0300

    Refactored ID.

--- selenium/ig.py
+++ selenium/ig.py
@@ -4,37 +4,38 @@ Description : Defines class Minas, child of Scraper, which scrapes the political
 Author      : Bernardo Paulsen
 Version     : 1.0.0
 """
-from   scraper import Scraper
+from scraper import Scraper
 
-#import line_profiler
-#import atexit
-#profile = line_profiler.LineProfiler()
-#atexit.register(profile.print_stats)
+
+# import line_profiler
+# import atexit
+# profile = line_profiler.LineProfiler()
+# atexit.register(profile.print_stats)
 
 class Ig(Scraper):
     """
     Class, child of Scraper, which scrapes the political section of Estado de Minas website.
     """
-    start_url   = "https://ultimosegundo.ig.com.br/politica/"
-    next_xpath  = "//a[@class='proxima']"
-    row_type    = "div"
-    row_class   = "empilhaImg-text"
+    start_url = "https://ultimosegundo.ig.com.br/politica/"
+    next_xpath = "//a[@class='proxima']"
+    row_type = "div"
+    row_class = "empilhaImg-text"
     title_xpath = "h2"
-    date_xpath  = "cite"
-    n_last      = 100
-    n_next      = 10
-    n_load      = 50
-    n_error     = 20
-    log_file    = "ig"
-    db_name     = "news.db"
-    db_table    = "ig"
-    
+    date_xpath = "cite"
+    n_last = 100
+    n_next = 10
+    n_load = 50
+    n_error = 20
+    log_file = "ig"
+    db_name = "news.db"
+    db_table = "ig"
+
     def get_row_xpath(self):
         return f"descendant-or-self::{self.row_type}[@class='{self.row_class}' and position()>last()-{self.n_last}]"
-    
+
     @staticmethod
     def get_date(
-        date : str):
+            date: str):
         """
         Gets date string and formats it to DATETIME data type (SQLite3).
 
