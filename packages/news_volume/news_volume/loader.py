87f851069b73e63b2f6ca661a0b9e137e33c885e
Bernardo Paulsen <bernardopaulsen@gmail.com>
2022-09-10 17:04:48 -0300

    remove comments on news_volume.loader

--- packages/news_volume/news_volume/loader.py
+++ packages/news_volume/news_volume/loader.py
@@ -9,6 +9,4 @@ class NewsLoader:
         with sqlite3.connect('../databases/news.db') as con:
             daily_news_volume = pd.read_sql_query("SELECT * FROM minas7",
                                                   con)
-                                                  # index_col='day',
-                                                  # parse_dates=['day'])
         return daily_news_volume
