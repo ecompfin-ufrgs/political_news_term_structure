34a9c57814306186e4a05540bebc47cf7803f901
Bernardo Paulsen <bernardopaulsen@gmail.com>
2022-09-12 20:02:11 -0300

    Generalized NewsLoader to support varios databases/tables.

--- packages/news_volume/news_volume/loader.py
+++ packages/news_volume/news_volume/loader.py
@@ -4,9 +4,16 @@ import sqlite3
 
 class NewsLoader:
 
+    def __init__(self,
+                 db_path: str,
+                 table: str
+                 ) -> None:
+        self.db_path = db_path
+        self.table = table
+
     def __call__(self):
 
-        with sqlite3.connect('../databases/news.db') as con:
-            daily_news_volume = pd.read_sql_query("SELECT * FROM minas7",
+        with sqlite3.connect(self.db_path) as con:
+            daily_news_volume = pd.read_sql_query(f'SELECT * FROM {self.table}',
                                                   con)
         return daily_news_volume
