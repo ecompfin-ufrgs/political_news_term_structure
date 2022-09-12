909aad751c82e75a9b49af3131b5648c433c94bf
Bernardo Paulsen <bernardopaulsen@gmail.com>
2022-09-12 20:15:45 -0300

    Generalized TermStructure to any spot rate csv path

--- packages/term_structure/term_structure/term_structure.py
+++ packages/term_structure/term_structure/term_structure.py
@@ -9,19 +9,16 @@ from .future_processor import FutureProcessor
 @dataclass
 class TermStructure:
 
-    dataframe_name: str
+    spot_rate_path: str
+    term_structure_path: str
 
     def __post_init__(self):
         self.spot = self._init_spot()
         self.future = self._init_future(spot_series=self.spot)
 
-    @staticmethod
-    def _init_spot(
+    def _init_spot(self
                    ) -> pd.Series:
-        # spot_df = dataframe.iloc[:, :1].dropna()
-        # spot_s = spot_df[spot_df.columns[0]]
-        # spot_s = spot_s.drop(spot_s[spot_s == '-'].index)
-        spot_df = pd.read_csv('di_anual.csv', sep=';')[:-1]
+        spot_df = pd.read_csv(self.spot_rate_path, sep=';')[:-1]
         spot_df.index = spot_df['Date'].apply(lambda x: dt.datetime.strptime(x, '%d/%m/%Y'))
         spot_s = spot_df[spot_df.columns[-1]]
         spot_s = spot_s.apply(lambda x: float(x))
@@ -30,7 +27,7 @@ class TermStructure:
     def _init_future(self,
                      spot_series: pd.Series
                      ) -> pd.DataFrame:
-        dataframe = pd.read_excel(self.dataframe_name, header=3, index_col=0)
+        dataframe = pd.read_excel(self.term_structure_path, header=3, index_col=0)
         future_df = dataframe.iloc[:, 1:]
         future_df = future_df.loc[spot_series.index, :]
         future_df = FutureProcessor()(dataframe=future_df)
