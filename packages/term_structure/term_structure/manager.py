701d10ce466c8c442065cbaf22058034b0fc3ad1
Bernardo Paulsen <bernardopaulsen@gmail.com>
2022-09-12 21:19:44 -0300

    Refactor term_structure

---
+++ packages/term_structure/term_structure/manager.py
@@ -0,0 +1,24 @@
+import typing as tp
+
+import pandas as pd
+
+from .spot_rate import SpotRate
+from .term_structure import TermStructure
+
+
+class TermStructureManager:
+
+    def __init__(self,
+                 spot_rate_path: str,
+                 term_structure_path: str
+                 ) -> None:
+        self.spot_rate_path = spot_rate_path
+        self.term_structure_path = term_structure_path
+
+    def __call__(self
+                 ) -> dict[str, tp.Union[pd.Series, pd.DataFrame]]:
+        spot = SpotRate(spot_rate_path=self.spot_rate_path)()
+        future = TermStructure(term_structure_path=self.term_structure_path,
+                               spot_rate_index=spot.index)
+        return dict(spot=spot,
+                    future=future)
--- packages/term_structure/term_structure/term_structure.py
+++ packages/term_structure/term_structure/term_structure.py
@@ -1,32 +1,24 @@
-from dataclasses import dataclass
-import datetime as dt
+import typing as tp
 
 import pandas as pd
 
 from .future_processor import FutureProcessor
-from .spot_rate import SpotRate
 
 
-@dataclass
 class TermStructure:
 
-    spot_rate_path: str
-    term_structure_path: str
+    def __init__(self,
+                 term_structure_path: str,
+                 spot_rate_index: tp.Optional[pd.Index] = None
+                 ) -> None:
+        self.term_structure_path = term_structure_path
+        self.spot_rate_index = spot_rate_index
 
-    def __post_init__(self):
-        self.spot = SpotRate(spot_rate_path=self.spot_rate_path)()
-        self.future = self._init_future(spot_series=self.spot)
-
-    def _init_future(self,
-                     spot_series: pd.Series
-                     ) -> pd.DataFrame:
+    def __call__(self
+                 ) -> pd.DataFrame:
         dataframe = pd.read_excel(self.term_structure_path, header=3, index_col=0)
         future_df = dataframe.iloc[:, 1:]
-        future_df = future_df.loc[spot_series.index, :]
+        if self.spot_rate_index is not None:
+            future_df = future_df.loc[self.spot_rate_index, :]
         future_df = FutureProcessor()(dataframe=future_df)
         return future_df
-
-
-
-
-
