077a357d876ac18363e2e9c0fd4ebf9bc17a427f
Bernardo Paulsen <bernardopaulsen@gmail.com>
2022-09-14 14:48:41 -0300

    Add plot for individual maturities.

--- scripts/term_structure_model/estimation.py
+++ scripts/term_structure_model/estimation.py
@@ -28,13 +28,20 @@ def plot_all_dates(df: pd.DataFrame):
     plt.clf()
 
 
+def plot_individual_maturity(df: pd.DataFrame):
+    df = df.T
+
+    for column in df.columns[::252]:
+        plt.plot(df[column], label=column)
+    plt.savefig('individual_maturity.png')
+    plt.clf()
+
+
 def main():
 
     term_structure = TermStructureManager(spot_rate_path='../../databases/di_anual.csv',
                                           term_structure_path='../../databases/economatica.xlsx')()
     vasicek_df = VasicekManager(term_structure=term_structure)()
-
-    print(vasicek_df)
 
 
 if __name__ == '__main__':
