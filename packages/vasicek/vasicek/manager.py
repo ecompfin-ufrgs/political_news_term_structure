06aa848e2304fd17867b699c148f5f8cb2af88e4
Bernardo Paulsen <bernardopaulsen@gmail.com>
2022-09-13 21:17:19 -0300

    Fix term_structure.manager

--- packages/vasicek/vasicek/manager.py
+++ packages/vasicek/vasicek/manager.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 import tqdm
 
-from ..market_data import TermStructure
-
 from .estimator import VasicekEstimator
 from .model import VasicekModel
 
@@ -13,15 +11,15 @@ class VasicekManager:
     MATURITY_ARRAY = np.array(range(1, 2000)) / 252
 
     def __init__(self,
-                 term_structure: TermStructure
+                 term_structure: dict
                  ) -> None:
         self.term_structure = term_structure
 
     def __call__(self
                  ) -> pd.DataFrame:
         df = pd.DataFrame()
-        for index, spot_rate in tqdm.tqdm(self.term_structure.spot.iteritems()):
-            term_s = self.term_structure.future.loc[index, :].dropna()
+        for index, spot_rate in tqdm.tqdm(self.term_structure['spot'].iteritems()):
+            term_s = self.term_structure['future'].loc[index, :].dropna()
             future_rate = term_s.to_numpy()
             future_time = np.array(list(term_s.index))/252
             try:
